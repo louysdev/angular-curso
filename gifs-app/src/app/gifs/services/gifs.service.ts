3eb5d5927c651c607d71cd60180432c16c036b95
Louys <113802499+louysdev@users.noreply.github.com>
2024-07-23 14:58:02 +0000

    Nuevas funcionalidades

--- gifs-app/src/app/gifs/services/gifs.service.ts
+++ gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,19 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
 
 // El servicio esta disponible para todos los components que inyecten este servicio
 @Injectable({ providedIn: 'root' })
 export class GifsService {
+  public gifList: Gif[] = [];
   private _tagsHistory: string[] = [];
+  private _serviceUrl = `https://api.giphy.com/v1/gifs`;
+  private _apiKey = 'TApdbVgs7S5QEPSORgO7lkYA9MPudfa5';
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadLocalStorage(); // LLamar cuando se crear el servicio, osea desde que carga la pagina
+    this.loadLastGist();
+  }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -14,16 +22,47 @@ export class GifsService {
   private organizeHistory(tag: string) {
     tag = tag.toLowerCase();
 
-    if(this._tagsHistory.includes(tag)) {
+    if (this._tagsHistory.includes(tag)) {
       this._tagsHistory = this._tagsHistory.filter((oldTag) => oldTag !== tag);
     }
+
+    this._tagsHistory.unshift(tag);
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+    this.saveLocalStorage();
   }
 
   searchTag(tag: string): void {
-    if(tag.length === 0) return;
+    if (tag.length === 0) return;
+    // Añadir tag al principio del array
     this.organizeHistory(tag);
 
-    // Añadir tag al principio del array
-    this._tagsHistory.unshift(tag);
+    // Parametros
+    const params = new HttpParams()
+      .set('api_key', this._apiKey)
+      .set('limit', '10')
+      .set('q', tag);
+
+    // Peticion http utilizando el modulo de angular
+    this.http
+      .get<SearchResponse>(`${this._serviceUrl}/search`, { params })
+      .subscribe((resp) => {
+        this.gifList = resp.data;
+      });
+  }
+
+  private saveLocalStorage(): void {
+    localStorage.setItem('history', JSON.stringify(this._tagsHistory));
+  }
+
+  private loadLocalStorage(): void {
+    if (!localStorage.getItem('history')) return;
+
+    this._tagsHistory = JSON.parse(localStorage.getItem('history')!);
+  }
+
+  private loadLastGist(): void {
+    if (this._tagsHistory.length === 0) return;
+
+    this.searchTag(this._tagsHistory[0]);
   }
 }
